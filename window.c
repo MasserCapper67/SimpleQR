1c98baeffea03812cad7e2c98071cbe7f58f39db
massercapper67 <alvarobardajirobles@gmail.com>
2026-01-30 01:31:02 +0100

    Now it is possible to draw pixels to the screen

--- window.c
+++ window.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
+static int window_height;
+static int window_width;
+static SDL_Window *window;
+static SDL_Renderer *renderer;
+
+static void draw_pixel(float x, float y, uint8_t r, uint8_t g, uint8_t b)
+{
+  if (x < 0 || x > window_width)
+  {
+    fprintf(stderr, "The value of x(%d) is out of the screen\n", x);
+    return;
+  }
+
+  if (y < 0 || y > window_height)
+  {
+    fprintf(stderr, "The value of y(%d) is out of the screen\n", y);
+    return;
+  }
+
+  SDL_SetRenderDrawColor(renderer, r, g, b, 255);
+  SDL_RenderPoint(renderer, x, y);
+}
+
 int main(int argc, char* argv[])
 {
-  SDL_Window *window;
-  SDL_Renderer *renderer;
   bool done = false;
   SDL_FRect rectangle = {
     .x = 0.0,
@@ -33,6 +55,12 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  if (!SDL_GetWindowSize(window, &window_width, &window_height))
+  {
+    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not get width and height from the screen: %s\n", SDL_GetError());
+    return 1;
+  }
+
   while (!done)
   {
     SDL_Event event;
@@ -46,8 +74,13 @@ int main(int argc, char* argv[])
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderRect(renderer, &rectangle);
+    for (int i = 0; i < 30; i++)
+    {
+      for (int j = 0; j < 30; j++)
+      {
+        draw_pixel(100 + j, 100 + i, 255, 255, 255);
+      }
+    }
 
     SDL_RenderPresent(renderer);
   }
@@ -58,3 +91,4 @@ int main(int argc, char* argv[])
   SDL_Quit();
   return 0;
 }
+
